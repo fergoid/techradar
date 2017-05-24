041565d12b77fd9ff3ace4bcb76dc0fd1c63f035
markferguson <mark.ferguson0@gmail.com>
2017-05-24 16:43:12 +0100

    Changed host

--- webpack.config.js
+++ webpack.config.js
@@ -75,8 +75,9 @@ module.exports = {
     devtool: devtool,
 
     devServer: {
+
         contentBase: buildPath,
-        host: '0.0.0.0',
+        host: '127.0.0.1',
         port: 8080
     }
 };
